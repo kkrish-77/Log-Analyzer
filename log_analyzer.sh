050b0ab8d2c54e502b4b2a1f3870fd2261185b55
kkrish-77 <kkrshsingh@gmail.com>
2025-10-28 20:43:33 +0530

    Add most requested log analyzer

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -6,3 +6,7 @@ echo "Analyzing log file: $LOG_FILE"
 
 echo "Number of 404 errors:"  # For 404 errors
 grep "404" $LOG_FILE | wc -l
+
+echo "Top 5 requested pages:" # For most requested pages
+awk '{print $7}' $LOG_FILE | sort | uniq -c | sort -nr | head -5
+
