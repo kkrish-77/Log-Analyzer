23b6fbeeec9de08e47eae74682a3f51711272b08
kkrish-77 <kkrshsingh@gmail.com>
2025-10-28 20:44:10 +0530

    Final commit

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -10,3 +10,5 @@ grep "404" $LOG_FILE | wc -l
 echo "Top 5 requested pages:" # For most requested pages
 awk '{print $7}' $LOG_FILE | sort | uniq -c | sort -nr | head -5
 
+echo "Top 5 IP addresses:" # For IPs with most requests
+awk '{print $1}' $LOG_FILE | sort | uniq -c | sort -nr | head -5
